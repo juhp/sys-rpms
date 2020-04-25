47f2c015ed64fc3a4b2388b0917c8b4c16a7c543
Jens Petersen <petersen@redhat.com>
2020-04-25 13:36:52 +0800

    diff, utc, but /proc/self/cgroup stops working for recent containers :(

--- Main.hs
+++ Main.hs
@@ -1,9 +1,10 @@
 module Main (main) where
 
+import Control.Monad
 import Data.Char
 import Data.List
+import Data.Time.Clock
 import Data.Time.Format
-import Data.Time.LocalTime
 import SimpleCmd
 import SimpleCmdArgs
 import System.Environment.XDG.BaseDir (getUserCacheDir)
@@ -20,14 +21,15 @@ main =
     [ Subcommand "save" "save rpm list for this system/container" $
       pure saveCmd
     , Subcommand "diff" "compare current installed rpms with saved list" $
-      pure diffCmd
-    -- , Subcommand "list" "compare current installed rpms with saved list" $
-    --   pure diffCmd
+      diffCmd <$> optional (strArg "ID")
+    , Subcommand "list" "list of rpm systems saved" $
+      pure listCmd
     ]
 
+-- FIXME --versions ?
 saveCmd :: IO ()
 saveCmd = do
-  basefile <- getCacheFile
+  basefile <- getCacheFile Nothing
   exists <- doesFileExist basefile
   rpms <- sort <$> cmdLines "rpm" rpmqaArgs
   mstore <-
@@ -35,8 +37,8 @@ saveCmd = do
       origrpms <- lines <$> readFile basefile
       if rpms == origrpms then return Nothing
         else do
-        zt <- getZonedTime
-        let timestamp = formatTime defaultTimeLocale (iso8601DateFormat (Just "%H:%M:%S")) zt
+        zt <- getCurrentTime
+        let timestamp = formatTime defaultTimeLocale "%Y-%m-%dT%H:%M:%S%z" zt
         return $ Just $ basefile <.> timestamp
     else return $ Just basefile
   case mstore of
@@ -46,15 +48,19 @@ saveCmd = do
       putStrLn store
 
 -- FIXME --force/--delete
-getCacheFile :: IO FilePath
-getCacheFile = do
-  cgroup <- takeBaseName <$> readFile "/proc/self/cgroup"
-  ident <- take 12 <$> case span (isLetter) cgroup of
-            ("libpod", cid) -> return $ tail cid
-            ("docker", cid) -> return $ tail cid
-            _ -> readFile "/etc/machine-id"
+getCacheFile :: Maybe String -> IO FilePath
+getCacheFile mid = do
+  ident <- maybe getSystemId return mid
   dir <- getUserCacheDir "sys-rpms"
   return $ dir </> ident
+
+getSystemId :: IO String
+getSystemId = do
+  cgroup <- takeBaseName <$> readFile "/proc/self/cgroup"
+  take 12 <$> case span (isLetter) cgroup of
+                ("libpod", cid) -> return $ tail cid
+                ("docker", cid) -> return $ tail cid
+                _ -> readFile "/etc/machine-id"
 
 latestCacheFile :: FilePath -> IO FilePath
 latestCacheFile path = do
@@ -62,11 +68,32 @@ latestCacheFile path = do
   files <- sort . filter (base `isPrefixOf`) <$> listDirectory dir
   return $ removePrefix base $ last files
 
-diffCmd :: IO ()
-diffCmd = do
-  basefile <- getCacheFile
+diffCmd :: Maybe String -> IO ()
+diffCmd mid = do
+  basefile <- getCacheFile mid
   latest <- latestCacheFile basefile
   pipe3_ ("rpm",rpmqaArgs) ("sort",[]) ("diff",["-u0", basefile ++ latest, "-"])
 
 rpmqaArgs :: [String]
 rpmqaArgs = ["-qa", "--qf", "%{name}\n"]
+
+listCmd :: IO ()
+listCmd = do
+  dir <- getUserCacheDir "sys-rpms"
+  files <- filter (not . ("." `isInfixOf`)) <$> listDirectory dir
+  machineid <- take 12 <$> readFile "/etc/machine-id"
+  ident <- getSystemId
+  forM_ files $ \ file -> do
+    putStr file
+    if file == machineid
+      then putStr $ " hostsystem"
+      -- FIXME use "--format {{.NAMES}}" when it is unbroken
+      else do
+      -- FIXME support docker too, anything else?
+      podman <- findExecutable "podman"
+      case podman of
+        Nothing -> putStr $ " container"
+        Just _ -> do
+          name <- last . words . last <$> cmdLines "podman" ["ps", "--filter", "id=" ++ file]
+          putStr $ " " ++ name
+    putStrLn $ if file == ident then " [local]" else ""
